266874efe186ce25ef198759878fb9c1cd2c9e36
JACOB DEATON <jrdeaton@SH308-03.local>
2013-01-22 12:01:50 -0600

    Tutorial No work

---
+++ BirdWatching/BirdWatching/BirdsMasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  BirdsMasterViewController.h
+//  BirdWatching
+//
+//  Created by JACOB DEATON on 1/22/13.
+//  Copyright (c) 2013 JACOB DEATON. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BirdsMasterViewController : UITableViewController
+
+@end
